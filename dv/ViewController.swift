ca164c21e5ff3140852b5dbdb576a35960e1688e
steven mankina <clay@stevens-MacBook-Pro.local>
2018-11-12 15:04:58 +0530

    xxc

--- dv/ViewController.swift
+++ dv/ViewController.swift
@@ -6,20 +6,14 @@
 //  Copyright © 2018 RoxAI. All rights reserved.
 //
 
+// our new gitHub repository... 
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
