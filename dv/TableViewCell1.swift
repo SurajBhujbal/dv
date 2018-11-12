973150422e6ae68a848ccc302e8b9481108ebe46
steven mankina <clay@stevens-MacBook-Pro.local>
2018-11-12 15:10:34 +0530

    dvxcx

---
+++ dv/TableViewCell1.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewCell1.swift
+//  dv
+//
+//  Created by steven mankina on 12/11/18.
+//  Copyright © 2018 RoxAI. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell1: UITableViewCell {
+
+    @IBOutlet weak var lbl1: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
